e4db9de0d102eed71de95d9182503ebbcc4c2a14
Akhireddy <akhir1989@gmail.com>
2021-12-08 21:56:32 +0800

    updated date

--- 03-Variable.sh
+++ 03-Variable.sh
@@ -26,3 +26,22 @@ ex:- $(a)
 
 echo a = $a
 echo a in currency = ${a}USD
+
+#usage
+#1.if we want to use a value in multiple places then we go with variables
+#2.this brings and advantage of changing the value in one place and it impact in multiple places one of the major advantage
+ #you can declare the variable to use it in multiple places.
+
+DATE=2021-12-08
+echo Good Morning, Welcome, Today date is ${DATE}
+##2. usecase: if we need to get the data dynamically we use variables to store that data and we refer
+  #1. Command substitution - VAR=$(command)
+  #2. Arthimetic substitution VAR=$((expression))
+
+#date help  run date help command it has surtence optins  %F we can see that date
+
+DATE=$(date +%F)
+echo good morning welcome, today date is ${DATE}
+
+ADD=$((2+3))
+echo ADD = ${ADD}
