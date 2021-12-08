527ed54db5f875b5c143f178cd8c5d6a357b1753
Akhireddy <akhir1989@gmail.com>
2021-12-08 17:45:49 +0800

    created a new print shell script file

---
+++ 02-Print.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#printing in shell can be done by using echo command
+
+#Syntax : echo INPUT MESSAGE
+
+echo Hello
+echo Hello World
+
+#Example from project
+echo Installing Nginx
+echo Starting Nginx service
