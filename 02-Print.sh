895aa1f4a34e17da1a34942e3c045d8562e1a607
Akhireddy <akhir1989@gmail.com>
2021-12-08 17:51:57 +0800

    updated print shell script file

--- 02-Print.sh
+++ 02-Print.sh
@@ -10,3 +10,22 @@ echo Hello World
 #Example from project
 echo Installing Nginx
 echo Starting Nginx service
+
+## we can print text in colors
+#Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+## -e -To enable \e
+## \e[ - to enable colors
+## COL - color code
+## m --end of syntax
+## 0 - to disable color
+## color codes
+# REd  31
+# Green 32
+# yellow 33
+# blue   34
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magenta Color\e[0m"
+echo -e "\e[31mHello in Red Color\e[0m"
