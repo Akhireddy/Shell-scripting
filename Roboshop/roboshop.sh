88c0d938e77deaecc6bf9c974a9e0b4e3afbe673
Akhireddy <akhir1989@gmail.com>
2021-12-09 20:17:23 +0800

    new file created

--- Roboshop/roboshop.sh
+++ Roboshop/roboshop.sh
@@ -13,7 +13,7 @@ if [ -z "COMPONENT" ]; then
 fi
 
 if [ ! -e components/${COMPONENT}.sh ]; then
-   echo -e "\e[1;31mgiven Component script does not exists\e[0m"
+   echo -e "\e[1;31mGiven Component script does not exists\e[0m"
    exit
 fi
 
