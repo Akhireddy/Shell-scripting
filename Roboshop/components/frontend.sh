5fb552859a3d938302b00b84af4737d0503d6b66
Akhireddy <akhir1989@gmail.com>
2021-12-13 00:50:13 +0800

    updated

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -24,12 +24,12 @@
 #rm -f ${LOG_FILE}
 
 #STAT_CHECK() {
-  if [ $1 -ne 0 ]; then
-     echo -e "\e[1;31m${2} - Failed\e[0m"
-     exit 1
-     else
-     echo -e "\e[1;32m${2} - SUCCESS\e[0m"
-   fi
+#  if [ $1 -ne 0 ]; then
+#    echo -e "\e[1;31m${2} - Failed\e[0m"
+#     exit 1
+#     else
+#     echo -e "\e[1;32m${2} - SUCCESS\e[0m"
+#   fi
 #}
 
 source components/common.sh
