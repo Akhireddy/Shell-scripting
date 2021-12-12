bb8d49357d0e0a57c0135dbbbc906a14d831621c
Akhireddy <akhir1989@gmail.com>
2021-12-12 19:24:06 +0800

    frintend script final

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -17,14 +17,35 @@
 #Finally restart the service once to effect the changes.
 
 # systemctl restart nginx
-yum install nginx -y
-if [ $? -ne 0 ]; then
-   echo "Nginx Install failed"
-   exit
-fi
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
-if [ $? -ne 0 ]; then
-   echo "Download contented is  failed"
-   exit
-fi
+
+LOG_FILE=/tmp/roboshop./log
+rm -f ${LOG_FILE}
+
+STAT_CHECK() {
+  if [ $1 -ne 0 ]; then
+     echo -e "\e[1;31m${2} - Failed\e[0m"
+     exit 1
+     else
+     echo -e "\e[1;32m${2} - SUCCESS\e[0m"
+   fi
+}
+
+yum install nginx -y >>${LOG_FILE}
+STAT_CHECK $? "Nginx installation"
+
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+STAT_CHECK $? "Dowload frontend failed"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl start nginx
+
+
 
